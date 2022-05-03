1216854968dc0eb73a9415daa96a2cb89473a320
SomeRandomTV <61666807+SomeRandomTV@users.noreply.github.com>
2022-05-02 19:21:34 -0600

    Create playerMovement.cpp

---
+++ playerMovement.cpp
@@ -0,0 +1,169 @@
+ switch (playerLocation) {
+        case 0:
+            nameLocation = "Go";
+            passgo = true;
+            break;
+        case 1:
+            
+                nameLocation = "Meditarianin Ave. \nPrice $60";
+                cost = 60;
+            
+           
+            break;
+        case 2:
+                nameLocation = "Community Chest ";
+                
+                
+            break;
+        case 3:
+                nameLocation = "Baltic Ave. \nPrice $60 ";
+                cost = 60;
+        
+            break;
+        case 4:
+                nameLocation = "Income Tax \nPay $200 ";
+            
+            break;
+        case 5:
+                nameLocation = "Reading Railroad. \nPrice $200 ";
+                cost = 200;
+            
+            break;
+        case 6:
+                nameLocation = "Oriental Ave. \nPrice $100";
+                cost = 200;
+    
+           
+            break;
+        case 7:
+                nameLocation = "Chance Card";
+
+            
+            break;
+        case 8:
+                nameLocation = "Just Visiting (unless you are bad boy)";
+
+            
+            break;
+        case 9:
+                nameLocation = "St. Charles Place \nPrice $140";
+                cost = 140;
+            
+            break;
+        case 10:
+                nameLocation = "Electric Company \nPay $150";
+                cost = 150;
+                
+            break;
+        case 11:
+                nameLocation = "States Ave. \nPrice $140";
+                cost = 140;
+           
+            break;
+        case 12:
+            
+            break;
+        case 13:
+            
+            break;
+        case 14:
+            
+            break;
+        case 15:
+            
+            break;
+        case 16:
+            
+            break;
+        case 17:
+            
+            break;
+        case 18:
+            
+            break;
+        case 19:
+            
+            break;
+        case 20:
+            
+            break;
+        case 21:
+            
+            break;
+        case 22:
+            
+            break;
+        case 23:
+            
+            break;
+        case 24:
+            
+            break;
+        case 25:
+            
+            break;
+        case 26:
+            
+            break;
+        case 27:
+           
+            break;
+        case 28:
+            
+            break;
+        case 29:
+            
+            break;
+        case 30:
+            
+            break;
+        case 31:
+            
+            break;
+        case 32:
+            
+            break;
+        case 33:
+            
+            break;
+        case 34:
+            
+            break;
+        case 35:
+            
+            break;
+        case 36:
+            
+            break;
+        case 37:
+            
+            break;
+        case 38:
+            
+            break;
+        case 39:
+            
+            break;
+        case 40:
+           
+            break;
+
+        }
+    
+}
+
+
+
+int Player::GetLocation() {
+    return playerLocation;
+}
+
+
+
+
+
+
+
+          
+                
+                
